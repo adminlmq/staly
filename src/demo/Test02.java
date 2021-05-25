16edf90c539149af3d73f801a1200a3a11502a4e
adminlmq <38445678+adminlmq@users.noreply.github.com>
2021-05-25 23:36:52 +0800

    Update Test02.java
    pull test commit

--- src/demo/Test02.java
+++ src/demo/Test02.java
@@ -6,5 +6,6 @@ public class Test02 {
         System.out.println("wosjo222");
         System.out.println("wosjo333");
         System.out.println("wosjo444");
+        System.out.println("pull");
     }
 }

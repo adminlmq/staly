d66809ef696d7fa4c60619041db3d70a5f769346
adminlmq <413840659@qq.com>
2021-05-25 23:41:44 +0800

    push test commit

--- src/demo/Test02.java
+++ src/demo/Test02.java
@@ -6,6 +6,7 @@ public class Test02 {
         System.out.println("wosjo222");
         System.out.println("wosjo333");
         System.out.println("wosjo444");
+        System.out.println("push");
         System.out.println("pull");
     }
 }
